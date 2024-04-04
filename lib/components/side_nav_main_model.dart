a66081ca7b7106c152776a4afbe05135b081f899
Isaque Burgheri <isaqueburgheri@gmail.com>
2024-04-04 14:06:37 -0300

    Add files via upload

---
+++ lib/components/side_nav_main_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'side_nav_main_widget.dart' show SideNavMainWidget;
+import 'package:flutter/material.dart';
+
+class SideNavMainModel extends FlutterFlowModel<SideNavMainWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
